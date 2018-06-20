6d0751f981d637d52906afe01d7ffd5e48b85d87
Jonathan Mezach <jonathanm@infosupport.com>
2018-06-20 10:32:56 +0200

    Added initial ATW project

---
+++ src/ATW/Specifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Augurk.Hackathon.ATW.Specifications")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Augurk.Hackathon.ATW.Specifications")]
+[assembly: AssemblyCopyright("Copyright © Augurk 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("35b931bf-7ccc-4846-9668-66bf5b5996a2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
