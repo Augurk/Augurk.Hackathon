10267ab7fd6d469c12103a2c909a8fa5ba4ef83f
Jonathan Mezach <jonathanm@infosupport.com>
2018-07-18 10:19:18 +0200

    Move steps to appropriate file

--- src/ATW/Specifications/Steps/MaximaleArbeidstijdRegelSteps.cs
+++ src/ATW/Specifications/Steps/MaximaleArbeidstijdRegelSteps.cs
@@ -1,5 +1,6 @@
 ﻿using Augurk.Hackathon.ATW.Regels;
 using Augurk.Hackathon.ATW.Specifications.Support;
+using System.Collections.Generic;
 using TechTalk.SpecFlow;
 
 namespace Augurk.Hackathon.ATW.Specifications.Steps
@@ -7,11 +8,21 @@ namespace Augurk.Hackathon.ATW.Specifications.Steps
     [Binding]
     public class MaximaleArbeidstijdRegelSteps
     {
+        private readonly List<(int leeftijd, int maximaleArbeidstijd)> maximaleArbeidstijden = new List<(int leeftijd, int maximaleArbeidstijd)>();
         private readonly ATWDriver _atw;
+        private readonly InrichtingProvider _inrichtingProvider;
 
-        public MaximaleArbeidstijdRegelSteps(ATWDriver atw)
+        public MaximaleArbeidstijdRegelSteps(ATWDriver atw, InrichtingProvider inrichtingProvider)
         {
             _atw = atw;
+            _inrichtingProvider = inrichtingProvider;
+        }
+
+        [Given(@"de maximaal toegestane arbeidstijd vanaf (.*) jaar is (.*) uur")]
+        public void GegevenDeMaximaalToegestaneArbeidstijdVanafJaarIsUur(int leeftijd, int maximaleArbeidstijd)
+        {
+            maximaleArbeidstijden.Add((leeftijd, maximaleArbeidstijd));
+            _inrichtingProvider.SetWaarden("MaximaleArbeidstijd", maximaleArbeidstijden.ToArray());
         }
 
         [When(@"de controle ""maximale arbeidstijd"" wordt uitgevoerd")]
